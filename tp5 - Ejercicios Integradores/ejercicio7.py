4ae2ced8be481f6539b1afc63debb3a6798e1187
ignacioDorigo <71302004+ignacioDorigo@users.noreply.github.com>
2025-02-05 23:28:36 -0300

    ejercicio 7terminado

--- tp5 - Ejercicios Integradores/ejercicio7.py
+++ tp5 - Ejercicios Integradores/ejercicio7.py
@@ -6,4 +6,17 @@
 # Por ejemplo, si se ingresa 1234 debe mostrar 4321.
 
 
-numero = int(input("Ingrese "))
+numero = int(input("INGRESE NUMERO PARA INVERTIR: "))
+print(f"ORIGINAL:{numero}")
+if(numero < 0):
+    numero = numero * -1
+
+reves = 0
+while(numero>0):
+    
+    ultimoDigito = numero % 10
+    reves = reves * 10 + ultimoDigito
+    numero = numero // 10
+    print(reves)
+    
+print(f"INVERTIDO:{reves}")
