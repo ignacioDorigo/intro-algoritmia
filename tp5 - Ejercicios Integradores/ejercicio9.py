475ceeabe233ed06ff9b7989bfafcf74109ad500
ignacioDorigo <71302004+ignacioDorigo@users.noreply.github.com>
2025-02-05 23:50:48 -0300

    ejericio 9 terminado

--- tp5 - Ejercicios Integradores/ejercicio9.py
+++ tp5 - Ejercicios Integradores/ejercicio9.py
@@ -10,3 +10,16 @@
 # Si se ingresa 8, se debe mostrar 1 3 5 7 y la suma es 16.
 
 # Si se ingresa -5, se debe pedir otro número.
+
+n = int(input("INGRESAR NUMERO ---> "))
+while (n<0):
+    print(f"EL NUMERO DEBE SER POSITIVO")
+    n = int(input("INGRESAR NUMERO ---> "))
+
+sumaImpares = 0
+for i in range(1,n+1,2):
+    sumaImpares = sumaImpares + i
+    print(i,end=" ")
+    
+print(sumaImpares)
+print()
