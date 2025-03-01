2dfdcfb058256d4088eb72fb014c7dee2526d50e
ignacioDorigo <71302004+ignacioDorigo@users.noreply.github.com>
2025-03-01 01:26:58 -0300

    Ejercicio 9 listo

--- tp6 - Funciones/ejercicio9.py
+++ tp6 - Funciones/ejercicio9.py
@@ -4,16 +4,20 @@
 # un número de día
 # un número de mes
 # un número de año
+# PUNTO  ---> A
 # devuelva la cantidad de días que faltan hasta fin de mes.
 
+# PUNTO  ---> B
 # Luego desarrollar tres programas para:
 # Ingresar una fecha formada por tres enteros (día, mes y año)
 # e imprimir la cantidad de días que faltan hasta fin de año.
 
+# PUNTO  ---> C
 # Ingresar una fecha formada por tres enteros (día, mes y año)
 # e imprimir la cantidad de días transcurridos
 # en ese año hasta esa fecha.
 
+# PUNTO  ---> D
 # Ingresar dos fechas formadas por tres enteros (día, mes y año)
 # e imprimir cuánto tiempo transcurrió entre ambas
 # expresando el resultado en años, meses y días.
@@ -56,6 +60,10 @@ def diasMes(mes,ano):
             return 28
     else:
         return 30
+    
+def diasHastaFinMes(dia,mes,ano):
+    diasDelMes = diasMes(mes,ano)
+    return (diasDelMes - dia)
     
 def validarFecha(dia,mes,ano):
     if(mes==2):
@@ -138,24 +146,116 @@ def diasAno(ano):
         return 366
     else:
         return 365
+    
+
+def diasDesdeF1hastaF2(d1,m1,a1,d2,m2,a2):
+    diaMayor = 0
+    mesMayor = 0
+    anoMayor = 0
+    diaMenor = 0
+    mesMenor = 0
+    anoMenor = 0
+    if (a1>a2):
+        diaMayor = d1
+        mesMayor = m1
+        anoMayor = a1
+        diaMenor = d2
+        mesMenor = m2
+        anoMenor = a2
+    elif (a2>a1):
+        diaMayor = d2
+        mesMayor = m2
+        anoMayor = a2
+        diaMenor = d1
+        mesMenor = m1
+        anoMenor = a1
+    else:
+        if(m1>m2):
+            diaMayor = d1
+            mesMayor = m1
+            anoMayor = a1
+            diaMenor = d2
+            mesMenor = m2
+            anoMenor = a2
+            
+        elif(m2>m1):
+            diaMayor = d2
+            mesMayor = m2
+            anoMayor = a2
+            diaMenor = d1
+            mesMenor = m1
+            anoMenor = a1
+        
+        else:
+            if(d1>d2):
+                diaMayor = d1
+                mesMayor = m1
+                anoMayor = a1
+                diaMenor = d2
+                mesMenor = m2
+                anoMenor = a2
+                
+            elif(d2>d1):
+                diaMayor = d2
+                mesMayor = m2
+                anoMayor = a2
+                diaMenor = d1
+                mesMenor = m1
+                anoMenor = a1
+                
+            else:
+                return 0
+    dias = 0
+    while (diaMenor!=diaMayor or mesMenor!=mesMayor or anoMenor!=anoMayor):
+        diaMenor,mesMenor,anoMenor = adelantarUnDia(diaMenor,mesMenor,anoMenor)
+        dias = dias + 1
+    
+    return dias
+            
 
 dia = pedirEntero(1,31,"INGRESE DIA --> ","ERROR: EL DIA DEBE ESTAR ENTRE 1 y 31")
 mes = pedirEntero(1,12,"INGRESE MES --> ","ERROR: EL MES DEBE ESTAR ENTRE 1 y 12")
 ano = pedirEntero(-99999,999999,"INGRESE AÑO ---> ","ERROR: EL AÑO DEBE ESTAR ENTRE -99999 Y 99999")
-result = validarFecha(dia,mes,ano)
-print(f"RESULTADO ---> {result}")
+# result = validarFecha(dia,mes,ano)
+# print(f"RESULTADO ---> {result}")
 
 d = dia
 m = mes
 a = ano
-print(f"{d}/{m}/{a}")
+
+print(f"FECHA 1 ---> {d}/{m}/{a}")
+print()
+
+
+# A
+print("PUNTO 1 --> DIAS HASTA FIN DE MES")
+print(f"DIAS HASTA FIN DE MES {diasHastaFinMes(d,m,a)}")
+print()
+
+# B
+print("PUNTO 2 --> DIAS HASTA FIN DE ANO ")
 diasTranscurridos = diasDesdeComienzoAno(d,m,a)
 diasHastaFinAno = diasAno(a) - diasTranscurridos
-# A
 print(f"DIAS HASTA FIN DE AÑO --> {diasHastaFinAno}")
-# B
-print(f"DIAS DEL AÑO TRANSCURRIDOS DESDE EL 1/1/{a} --> {diasTranscurridos}")
-# C
-print("FALTAA ..........")
+print()
 
-        
+# C
+print("PUNTO 3 --> DIAS DEL ANO TRANSCURRIDOS")
+print(f"DIAS DEL AÑO TRANSCURRIDOS DESDE EL 1/1/{a} --> {diasTranscurridos}")
+print()
+
+# D
+print("PUNTO 3 --> DIAS DEL ANO TRANSCURRIDOS")
+dia2 = pedirEntero(1,31,"INGRESE DIA --> ","ERROR: EL DIA DEBE ESTAR ENTRE 1 y 31")
+mes2 = pedirEntero(1,12,"INGRESE MES --> ","ERROR: EL MES DEBE ESTAR ENTRE 1 y 12")
+ano2 = pedirEntero(-99999,999999,"INGRESE AÑO ---> ","ERROR: EL AÑO DEBE ESTAR ENTRE -99999 Y 99999")
+print(f"FECHA 2 ---> {dia}/{mes2}/{ano2}")
+diasDif = diasDesdeF1hastaF2(d,m,a,dia2,mes2,ano2)
+anosDif = diasDif // 365
+diasDif = diasDif % 365
+mesesDif = diasDif // 30
+diasDif = diasDif % 30
+print("TIEMPO DE DIFERENCIA")
+print(f"ANOS: {anosDif}")
+print(f"MESES: {mesesDif}")
+print(f"DIAS: {diasDif}")
